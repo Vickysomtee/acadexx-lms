4622d6e9e9678fd9efc2ecd244c313b30720afce
Blessing Mba <blessingmba3@gmail.com>
2023-03-10 11:44:00 +0000

    changes

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,26 +1,28 @@
 <template>
-  <div class="flex space-between bg-white h-screen w-full">
-    <div class=" w-1/2  border-r-black-200  border-4 ">
-      <div class="mt-48 ml-24">
+  <div class="flex space-between bg-white h-full w-full full">
+    <div class=" w-1/2 left">
+      <div class="mt-48 ml-24 ">
         <img src="../assets/sitting-dev.svg" alt="">
       </div>
     </div>
     <div class=" w-1/2">
        <h3 class="mt-8 text-4xl">Create Account</h3>
-         <div>
+         <div class="mt-12">
           <form action="">
             <div class="mb-5">
            <label for="name" class="block mb-2 font-bold  text-lg text-left ml-20"> School Name</label>
            <input type="text" id="name" name="name" 
            class="border border-gray-300 shadow p-3 w-4/5 rounded mb-">
-           <label for="name" class="block mt-8 font-bold text-gray-600 text-left ml-20">Email Address</label>
+           <label for="name" class="block mt-8 font-bold text-lg text-gray-600 text-left ml-20">Email Address</label>
            <input type="text" id="email" name="email"
             class="border border-gray-300 shadow p-3 w-4/5 rounded mb-">
-
-            <label class="block mb-1 text-sm" for="select-item">Country:</label>
+            <label for="name" class="block mt-8 font-bold text-lg text-gray-600 text-left ml-20">Password</label>
+           <input type="text" id="password" name="password"
+            class="border border-gray-300 shadow p-3 w-4/5 rounded mb-">
+            <label class="block mb-1 text-lg font-bold text-left ml-20 mt-8" for="select-item">Country</label>
 
   <div class="relative w-4/5 ml-20">
-    <select class="w-full border px-4 pr-8 py-2 rounded focus:border-blue-500 focus:shadow-outline outline-none appearance-none" id="select-item">
+    <select class="w-full border border-gray-300 shadow p-3 rounded focus:border-blue-500 focus:shadow-outline outline-none appearance-none" id="select-item">
       <option>Singapore</option>
       <option>Luxembourg</option>
       <option>Italy</option>
@@ -38,10 +40,10 @@
     </div>
   </div>
 
-  <label class="block mb-1 text-sm" for="select-item">State:</label>
+  <label class="block mb-1 font-bold text-lg text-left ml-20 mt-8" for="select-item">State</label>
 
   <div class="relative  w-4/5 ml-20">
-    <select class="w-full border px-4 pr-8 py-2 rounded focus:border-blue-500 focus:shadow-outline outline-none appearance-none" id="select-item">
+    <select class="w-full border  border-gray-300 shadow p-3 focus:shadow-outline outline-none appearance-none" id="select-item">
       <option>Singapore</option>
       <option>Luxembourg</option>
       <option>Italy</option>
@@ -58,15 +60,32 @@
       </svg>
     </div>
   </div>
-  <label for="name" class="block mb-2 font-bold  text-lg text-left ml-20"> Address</label>
+  <label for="name" class="block mb-2 font-bold  text-lg text-left ml-20 mt-8"> Address</label>
            <input type="text" id="address" name="address" 
            class="border border-gray-300 shadow p-3 w-4/5 rounded mb-">
          </div>
           </form>
+          <button class="mt-12 text-white text-2xl rounded-xl font-semibold ">Create</button>
          </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+   button{
+    background:  #3503FD;
+    width: 356px;
+    height: 73px;
+   }
+   .left{
+    border-right: 1px solid #7A7A7A; 
+   }
+   .full{
+    border-bottom-left-radius: 40%;
+   }
+</style>
+
+
 
 <script>
 export default {
